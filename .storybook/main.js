fd94e2b8b7f114ea093397e20cad4eb359bebe61
Ariene Maiara <ariene.maiara@hotmail.com>
2020-09-22 20:31:44 -0300

    Add theme provider to styled components

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
   stories: ['../src/components/**/stories.[tj]sx'],
-  addons: ['@storybook/addon-essentials']
+  addons: ['@storybook/addon-essentials'],
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`)
+    return config
+  }
 }
