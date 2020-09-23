e11a5354c95612bb8caadf46da4a2064783d03ad
Ariene Maiara <ariene.maiara@hotmail.com>
2020-09-22 21:17:42 -0300

    Test logo render with different colors

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,8 @@ module.exports = {
   testPathIgnorePatterns: ['/node_modules/', './next/'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: [
+    'src',
+  ],
 }
