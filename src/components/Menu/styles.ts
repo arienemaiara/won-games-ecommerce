57fee051d755f091edebdaeef355cb2a889cfd8d
Ariene Maiara <ariene.maiara@hotmail.com>
2020-09-23 21:57:39 -0300

    Add style to menu desktop

--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -31,6 +31,7 @@ export const MenuGroup = styled.div`
   ${({ theme }) => css`
     display: flex;
     flex-grow: 1;
+    align-items: center;
     justify-content: flex-end;
 
     > div {
@@ -38,13 +39,18 @@ export const MenuGroup = styled.div`
     }
   `}
 `
-export const MenuNav = styled.div``
+export const MenuNav = styled.div`
+  ${({ theme }) => css`
+    margin-left: ${theme.spacings.small};
+  `}
+`
 
 export const MenuLink = styled.a`
   ${({ theme }) => css`
     position: relative;
     font-size: ${theme.font.sizes.medium};
     margin: 0.3rem ${theme.spacings.small} 0;
+    color: ${theme.colors.white};
     text-decoration: none;
     text-align: center;
 
