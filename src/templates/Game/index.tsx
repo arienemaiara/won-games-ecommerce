34874c8ae10fc4c7e863fe782b850730f48605e9
arienemaiara <ariene.maiara@hotmail.com>
2021-01-31 22:05:55 -0300

    Add game details

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -3,6 +3,7 @@ import Base from 'templates/Base'
 import GameInfo, { GameInfoProps } from 'components/GameInfo'
 import Gallery, { GalleryImageProps } from 'components/Gallery'
 import TextContent from 'components/TextContent'
+import GameDetails, { GameDetailsProps } from 'components/GameDetails'
 
 import * as S from './styles'
 
@@ -11,9 +12,10 @@ export type GameTemplateProps = {
   gameInfo: GameInfoProps
   gallery?: GalleryImageProps[]
   description: string
+  details: GameDetailsProps
 }
 
-const Game = ({ cover, gameInfo, gallery, description }: GameTemplateProps) => (
+const Game = ({ cover, gameInfo, gallery, description, details }: GameTemplateProps) => (
   <Base>
     <S.Cover src={cover} role="image" aria-label="cover" />
 
@@ -21,14 +23,21 @@ const Game = ({ cover, gameInfo, gallery, description }: GameTemplateProps) => (
       <S.SectionGameInfo>
         <GameInfo {...gameInfo} />
       </S.SectionGameInfo>
+
       {!!gallery && (
         <S.SectionGallery>
           <Gallery items={gallery} />
         </S.SectionGallery>
       )}
+
       <S.SectionDescription>
         <TextContent title="Description" content={description} />
       </S.SectionDescription>
+
+      <S.SectionGameDetails>
+        <GameDetails {...details} />
+      </S.SectionGameDetails>
+
     </S.Main>
   </Base>
 )
